9390e55328ee707924fefd1648e8ac71c3b602ff
Sarath Radhakrishnan <sarathrk1@outlook.com>
2022-02-16 21:45:54 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const Connection = require('mysql/lib/Connection');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//Parsing Middleware
+//Parse application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Parse application/json
+app.use(bodyParser.json());
+
+//Static files
+app.use(express.static('public'));
+
+//Templating Engine
+app.engine('hbs', exphbs.engine( {extname:'.hbs' }));
+app.set('view engine','hbs');
+
+//Connection Pool
+const pool = mysql.createPool({
+    connectionLimit:100,
+    host           :process.env.DB_HOST,
+    user           :process.env.DB_USER,
+    password       :process.env.DB_PASS,
+    database       :process.env.DB_NAME
+});
+
+//Connection to Database
+pool.getConnection((err,connection)=>{
+    if(err) throw err;//If not connected properly
+    console.log('Connected as ID'+connection.threadId);
+});
+
+
+const routes = require('./server/routes/user');
+app.use('/',routes);
+
+
+app.listen(port,()=>console.log(`Listening on port ${port}`));
---
+++ server/controllers/userController.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+
+//Connection Pool
+const pool = mysql.createPool({
+    connectionLimit:100,
+    host           :process.env.DB_HOST,
+    user           :process.env.DB_USER,
+    password       :process.env.DB_PASS,
+    database       :process.env.DB_NAME
+});
+
+
+
+
+//view users
+exports.view = (req,res)=>{
+
+    pool.getConnection((err,connection)=>{
+        if(err) throw err;//If not connected properly
+        console.log('Connected as ID'+connection.threadId);
+
+        //Use the connection
+        connection.query('SELECT * FROM students',(err,rows)=>{
+            connection.release();
+
+            if(!err){
+                res.render('home',{ rows });
+            }else{
+                console.log(err);
+            }
+
+            console.log('The data from student table :\n',rows);
+
+        });
+});
+
+
+
+}
