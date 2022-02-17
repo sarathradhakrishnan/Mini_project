cab253311656fc656101812c0e11d588cf019d96
Sarath Radhakrishnan <sarathrk1@outlook.com>
2022-02-17 12:20:07 +0530

    Added edit feature

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -78,3 +78,65 @@ exports.create = (req,res)=>{
     });
     
 }
+
+//Edit user
+exports.edit = (req,res)=>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err;//If not connected properly
+        console.log('Connected as ID'+connection.threadId);
+        //let searchTerm = req.body.search;
+        
+        //Use the connection
+        connection.query('SELECT * FROM students WHERE Rollno = ?',[req.params.id],(err,rows)=>{
+            connection.release();
+
+            if(!err){
+                res.render('edit-user',{rows});
+                console.log(rows);
+            }else{
+                console.log(err);
+            }
+            console.log('The data from student table :\n',rows);
+        });
+    });
+}
+
+
+//Update User
+exports.update = (req,res)=>{
+    const {roll_no , class1 , first_name , last_name , email ,phone } = req.body;
+    pool.getConnection((err,connection)=>{
+        if(err) throw err;//If not connected properly
+        console.log('Connected as ID'+connection.threadId);
+        let searchTerm = req.body.search;
+        
+        //Use the connection
+        connection.query('UPDATE students SET first_name = ?, last_name = ? WHERE Rollno = ?',[first_name, last_name, req.params.id],(err,rows)=>{
+            connection.release();
+
+            if(!err){
+                pool.getConnection((err,connection)=>{
+                    if(err) throw err;//If not connected properly
+                    console.log('Connected as ID'+connection.threadId);
+                    //let searchTerm = req.body.search;
+                    
+                    //Use the connection
+                    connection.query('SELECT * FROM students WHERE Rollno = ?',[req.params.id],(err,rows)=>{
+                        connection.release();
+            
+                        if(!err){
+                            res.render('edit-user',{rows});
+                            console.log(rows);
+                        }else{
+                            console.log(err);
+                        }
+                        console.log('The data from student table :\n',rows);
+                    });
+                });
+            }else{
+                console.log(err);
+            }
+            console.log('The data from student table :\n',rows);
+        });
+    });
+}
