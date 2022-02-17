a789b589cbba94c6fdf7be345f601c2b439513ab
Sarath Radhakrishnan <sarathrk1@outlook.com>
2022-02-17 12:48:03 +0530

    Fixed a bug

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,14 +5,11 @@ const userController = require('../controllers/userController');
 
 router.get('/',userController.view);
 router.post('/',userController.find);
-
-router.get('/:id',userController.delete);
-
-
 router.get('/adduser',userController.form);
 router.post('/adduser',userController.create);
 router.get('/edituser/:id',userController.edit);
 router.post('/edituser/:id',userController.update);
+router.get('/:id',userController.delete);
 
 
 
